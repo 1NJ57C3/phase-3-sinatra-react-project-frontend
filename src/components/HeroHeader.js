3d9ca585019ee2aed7c65b4e4c528c2e4e48efff
Hannah McCullough <hmac091@gmail.com>
2022-01-26 19:00:16 -0700

    I don't even know

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import FadeIn from 'react-fade-in';
+
 function HeroHeader() {
 
     return (
         <div className='hero-img'>
-            <div className='overlay'>
+            <div className='hero-overlay'>
                 <div className='hero-content'>
-                    <img src='/cmLogo2.png' />
-                    <p id='slogan'>
-                        Trying to make the world a little less angry.
-                    </p>
+                    <FadeIn delay="1000" transitionDuration="3000">
+                        <img src='/cmLogo2.png'/>
+                    </FadeIn>
+                        <img src='/line.png'/>
+                    <FadeIn delay="2000" transitionDuration="2000">
+                        <p id='slogan-text'>
+                            Making the world a little less angry.
+                        </p>
+                    </FadeIn>
                 </div>
             </div>
         </div>
