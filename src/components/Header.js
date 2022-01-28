3165d0820bb9ad773bd32a7dc52a1e76cbefdbf1
Hannah McCullough <hmac091@gmail.com>
2022-01-28 14:12:36 -0700

    minor css change

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
                 <img src='/line.png'/>
             <FadeIn delay="1000" transitionDuration="2000">
                 <p id='slogan-text'>
-                    Making the world a little less angry.
+                    Making the world a little less angry one coffee recipe at a time.
                 </p>
             </FadeIn>
         </div>
